cae65e11ee966289da77a77a8f62faf8ec7c1dee
mayarrrrrr <akokmayar607@gmail.com>
2023-12-18 11:32:24 +0300

    Completed the assignment

--- speedDetector.js
+++ speedDetector.js
@@ -1,15 +1,14 @@
-// Here, am going to declare the general limits and points.
+
 
 function speedDetector(speed) {
     const maxSpeed = 70;
     const speedPerDemerit = 5;
     const maxDemerit = 12;
 
-    // Now am going to create the arguments.
     if (speed <= maxSpeed) {
         console.log("Ok");
     } else {
-        // And here, I code the arguments on how to calculate the demerit points.
+       
         const demeritPoints = Math.floor((speed - maxSpeed) / speedPerDemerit);
 
         if (demeritPoints >= maxDemerit) {
@@ -20,5 +19,5 @@ function speedDetector(speed) {
     }
 }
 
-// Example usage:
+
 speedDetector(130);
